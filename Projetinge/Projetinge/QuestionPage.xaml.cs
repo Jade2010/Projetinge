129d3cf7579c53ac4c03e0e3b9e45523f430280e
Helynna <elodie.charitat@gmail.com>
2019-06-05 14:44:38 +0200

    3 pages created

---
+++ Projetinge/Projetinge/QuestionPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projetinge
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuestionPage : ContentView
+    {/*label_
+         * bouton1
+         * bouton2
+         * bouton3
+         * bouton4
+         * 
+         */
+
+        public QuestionPage()
+        {
+            InitializeComponent();
+
+        }
+
+        /*role : sert à changer les thèmes marqués sur les boutons et les couleurs 
+         * entrée : 5 string qui correspondent aux thèmes à marquer sur les boutons et le label qui est pour la question
+         * sortie : vide
+         */
+        public void changerQuestion(String question,String Sbouton1, String Sbouton2, String Sbouton3, String Sbouton4)
+        {
+            label_.Text = question;
+            bouton1.Text = Sbouton1;
+            bouton2.Text = Sbouton2;
+            bouton3.Text = Sbouton3;
+            bouton4.Text = Sbouton4;
+
+            /*bouton1.BackgroundColor = ;
+             * bouton2.BackgroundColor = ;
+             * bouton3.BackgroundColor = ;
+             * bouton4.BackgroundColor = ;
+            */
+        }
+    }
+}
--- Projetinge/Projetinge/ThemePage.xaml.cs
+++ Projetinge/Projetinge/ThemePage.xaml.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Projetinge
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ThemePage : ContentView
     {
         /*label_
          * bouton1
@@ -17,7 +20,7 @@ namespace Projetinge
          * 
          */
 
-        public MainPage()
+        public ThemePage()
         {
             InitializeComponent();
 
@@ -29,11 +32,16 @@ namespace Projetinge
          */
         public void changerTheme(String Sbouton1, String Sbouton2, String Sbouton3, String Sbouton4, Color Cbouton1, Color Cbouton2, Color Cbouton3, Color Cbouton4)
         {
-           
+
+            bouton1.Text = Sbouton1;
+            bouton2.Text = Sbouton2;
+            bouton3.Text = Sbouton3;
+            bouton4.Text = Sbouton4;
+
+            bouton1.BackgroundColor = Cbouton1;
+            bouton2.BackgroundColor = Cbouton2;
+            bouton3.BackgroundColor = Cbouton3;
+            bouton4.BackgroundColor = Cbouton4;
         }
-
-
-
-
     }
 }
