092d8a74304fd1a9bed573b04d314ddb269fd468
Helynna <elodie.charitat@gmail.com>
2019-06-13 10:14:27 +0200

    mainPage done with logo & background

--- Projetinge/Projetinge/MainPage.xaml.cs
+++ Projetinge/Projetinge/MainPage.xaml.cs
@@ -20,7 +20,13 @@ namespace Projetinge
         public MainPage()
         {
             InitializeComponent();
-            
+            Image background = new Image();
+            background.Source= Device.RuntimePlatform == Device.Android ? ImageSource.FromFile("DecoderBackground.png") : ImageSource.FromFile("Images/DecoderBackground.png");
+
+            String test = "DecoderBackground.png";
+            BackgroundImage = test;
+            //imageLogo.Source = Device.RuntimePlatform == Device.Android ? ImageSource.FromFile("logo.png") : ImageSource.FromFile("Images/logo.png");
+            //imageLogo.Source = Device.RuntimePlatform == Device.Android ? ImageSource.FromFile("Images/logo.png") : ImageSource.FromFile("Drawable/Images/logo.png");
 
         }
 
